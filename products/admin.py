5cf6fc249a4c6d5218fe0ef89bd15d02c3178cd8
Madhav Paudel <poudelmadhav143@gmail.com>
2020-08-04 00:03:24 +0545

    offer model admin customize

--- products/admin.py
+++ products/admin.py
@@ -6,5 +6,9 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'stock')
 
 
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ('code', 'discount')
+
+
 admin.site.register(Product, ProductAdmin)
-admin.site.register(Offer)
+admin.site.register(Offer, OfferAdmin)
